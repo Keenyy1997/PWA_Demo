e3f214fa0a56931b4eb2256c8f84d3608220d6ea
Keenyy1997 <Keenyy1997@gmail.com>
2018-09-14 22:24:20 -0500

    Hello

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ if( 'serviceWorker' in navigator)
 {	
 	window.addEventListener('load',()=>
 	{
-		navigator.serviceWorker.register('./sw.js')
+		navigator.serviceWorker.register('/PWA_Demo/sw.js')
 			.then( registration =>
 			{
 				console.log("Service Worker Installing")
