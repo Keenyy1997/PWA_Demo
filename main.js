b3c03c7f71a844c86f87402c35df74d57faa5c64
Keenyy1997 <Keenyy1997@gmail.com>
2018-09-14 22:06:35 -0500

    Saving files

---
+++ main.js
@@ -0,0 +1,37 @@
+
+// SI SOPORTA "serviceWorker" in navigator
+if( 'serviceWorker' in navigator)
+{	
+	window.addEventListener('load',()=>
+	{
+		navigator.serviceWorker.register('scripts/sw.js')
+			.then( registration =>
+			{
+				console.log("Service Worker Installing")
+			})
+			.catch( error =>
+			{
+				console.log(error)
+			})
+
+		navigator.serviceWorker.ready.then(function(registration) {
+           console.log('Service Worker Ready');
+        })
+	})
+}
+
+if( window.Notification && Notification.permission !== 'denied' )
+{
+	// if(Notification.permission !== 'granted')
+	// {
+		Notification.requestPermission(status => 
+		{
+			console.log(status)
+		})
+	// }
+}
+
+const NotificationTest = () =>
+{
+	new Notification("Hi!")
+}
---
+++ sw.js
@@ -0,0 +1,40 @@
+const version = "0.1";
+const cacheName = `pwa-${version}`;
+self.addEventListener('install', e => {
+  const timeStamp = Date.now();
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+	// "./",
+	// "../",
+	"../index.html?utm=homescreen",
+	"../styles/main.css",
+	"../scripts/main.js",
+	"../scripts/promises.js",
+	"../scripts/sw.js",
+	"../favicon.ico",
+	"https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+
+	console.log("Activate")
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+
+	console.log("fetch")
+
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
